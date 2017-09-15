0aa380629707b73d526e499578e50494df68dbe3
Harry Nguon <harrynguon@gmail.com>
2017-09-15 17:42:03 +1200

    NPC class

---
+++ DungeonRaider/src/dungeonraider/character/NPC.java
@@ -0,0 +1,98 @@
+package dungeonraider.character;
+
+import java.awt.Image;
+import java.util.List;
+
+import dungeonraider.item.Item;
+
+public class NPC implements Character {
+	
+	private String name;
+	private String description;
+	private int coin; 
+	private List<Item> inventory;
+	private Image spriteImage;
+	
+	/**
+	 * NPC Constructor
+	 */
+	public NPC() {
+		
+	}
+	
+	/**
+	 * This method is for when a user interacts with this NPC. A JDialog will
+	 * pop up.
+	 */
+	public void interact() {
+		
+	}
+	
+	/**
+	 * Gives the item(s) that the user requests
+	 */
+	public void trade() {
+		
+	}
+
+	@Override
+	public void lightAttack() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void heavyAttack() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void walkLeft() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void walkRight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void walkUp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void walkDown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void runLeft() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void runRight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void runUp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void runDown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
