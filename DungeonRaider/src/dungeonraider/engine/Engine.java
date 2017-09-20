faca5c2fd86319c9abb8019c5348dee563abe5ff
Jono Yan <jono2496@gmail.com>
2017-09-20 19:22:28 +1200

    Modified Engine.java

--- DungeonRaider/src/dungeonraider/engine/Engine.java
+++ DungeonRaider/src/dungeonraider/engine/Engine.java
@@ -1,8 +1,11 @@
 package dungeonraider.engine;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Toolkit;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
@@ -25,11 +28,36 @@ public class Engine extends JFrame implements Runnable {
 		/** Close program on exit */
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		/** Set location of JFrame window and size */
-		setBounds(0, 0, (int) tk.getScreenSize().getWidth() / 2, (int) tk.getScreenSize().getHeight() / 2);
+		setBounds(0, 0, 1000, 1000);
 		/** Sets window to center */
 		setLocationRelativeTo(null);
+		/** Adds canvas to JFrame */
 		add(canvas);
+		/** Sets JFrame to visible */
 		setVisible(true);
+		/** Creates 3 buffer renderer */
+		canvas.createBufferStrategy(3);
+		BufferStrategy b = canvas.getBufferStrategy();
+		int i = 0;
+		int x = 0;
+		while (true) {
+			i++;
+			if (i == 10) {
+				i = 0;
+				x++;
+			}
+			b = canvas.getBufferStrategy();
+			Graphics gr = b.getDrawGraphics();
+			super.paint(gr);
+			gr.setColor(Color.blue);
+			gr.fillOval(x, 100, 100, 100);
+			gr.dispose();
+			b.show();
+		}
+	}
+
+	public void render() {
+
 	}
 
 	@Override
