5b3ee2e60a7cf0026646ec9c830f0c9c1d26ca93
Jono Yan <jono2496@gmail.com>
2017-09-20 19:29:33 +1200

    Modified Engine.java

--- DungeonRaider/src/dungeonraider/engine/Engine.java
+++ DungeonRaider/src/dungeonraider/engine/Engine.java
@@ -19,6 +19,8 @@ public class Engine extends JFrame implements Runnable {
 
 	private Canvas canvas;
 	private Toolkit tk;
+	private int width = 1000;
+	private int height = 800;
 
 	public Engine() {
 		this.canvas = new Canvas();
@@ -28,7 +30,7 @@ public class Engine extends JFrame implements Runnable {
 		/** Close program on exit */
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		/** Set location of JFrame window and size */
-		setBounds(0, 0, 1000, 1000);
+		setBounds(0, 0, width, height);
 		/** Sets window to center */
 		setLocationRelativeTo(null);
 		/** Adds canvas to JFrame */
@@ -37,6 +39,46 @@ public class Engine extends JFrame implements Runnable {
 		setVisible(true);
 		/** Creates 3 buffer renderer */
 		canvas.createBufferStrategy(3);
+		// BufferStrategy b = canvas.getBufferStrategy();
+		// int i = 0;
+		// int x = 0;
+		// while (true) {
+		// i++;
+		// if (i == 10) {
+		// i = 0;
+		// x++;
+		// }
+		// b = canvas.getBufferStrategy();
+		// Graphics gr = b.getDrawGraphics();
+		// super.paint(gr);
+		// gr.setColor(Color.blue);
+		// gr.fillOval(x, 100, 100, 100);
+		// gr.dispose();
+		// b.show();
+		// }
+	}
+
+	/**
+	 * This method will render everything onto the screen
+	 */
+	public void render() {
+
+	}
+
+	/**
+	 * This method will update to the buffer. EG. char movement
+	 * 
+	 * This method will run at a specified speed.
+	 */
+	public void update() {
+
+	}
+
+	/**
+	 * Thread will execute this code and run the game.
+	 */
+	@Override
+	public void run() {
 		BufferStrategy b = canvas.getBufferStrategy();
 		int i = 0;
 		int x = 0;
@@ -54,14 +96,6 @@ public class Engine extends JFrame implements Runnable {
 			gr.dispose();
 			b.show();
 		}
-	}
-
-	public void render() {
-
-	}
-
-	@Override
-	public void run() {
 
 	}
 
