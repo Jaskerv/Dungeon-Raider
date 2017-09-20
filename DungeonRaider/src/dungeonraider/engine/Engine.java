0990a75bdf84a02895744e58cf23ef42e2299b48
Jono Yan <jono2496@gmail.com>
2017-09-20 19:10:54 +1200

    Modified Engine.java

--- DungeonRaider/src/dungeonraider/engine/Engine.java
+++ DungeonRaider/src/dungeonraider/engine/Engine.java
@@ -1,6 +1,8 @@
 package dungeonraider.engine;
 
+import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
@@ -10,23 +12,35 @@ import javax.swing.JFrame;
  * @author Jono Yan
  *
  */
-public class Engine implements Runnable {
-	private JFrame view;
+public class Engine extends JFrame implements Runnable {
+
+	private Canvas canvas;
+	private Toolkit tk;
 
 	public Engine() {
-		this.view = new JFrame("Dungeon Raider");
-		this.view.setVisible(true);
+		this.canvas = new Canvas();
+		this.tk = this.getToolkit();
+		/** Sets name of JFrame window */
+		setTitle("Dungeon Raider");
+		/** Close program on exit */
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		/** Set location of JFrame window and size */
+		setBounds(0, 0, (int) tk.getScreenSize().getWidth() / 2, (int) tk.getScreenSize().getHeight() / 2);
+		/** Sets window to center */
+		setLocationRelativeTo(null);
+		add(canvas);
+		setVisible(true);
 	}
 
 	@Override
 	public void run() {
-		
 
 	}
 
 	public static void main(String[] args) {
 		Engine game = new Engine();
-		game.run();
+		Thread thread = new Thread(game);
+		thread.start();
 	}
 
 }
