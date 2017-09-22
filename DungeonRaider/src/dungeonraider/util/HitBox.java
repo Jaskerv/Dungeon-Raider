9ae4685ed7a660701d6fe5dc2f170aef36f5b351
Jono Yan <jono2496@gmail.com>
2017-09-22 18:26:15 +1200

    Added hitboxes

---
+++ DungeonRaider/src/dungeonraider/util/HitBox.java
@@ -0,0 +1,41 @@
+package dungeonraider.util;
+
+public class HitBox {
+	private Position pos;
+	private int width;
+	private int height;
+
+	public HitBox(int x, int y, int width, int height) {
+		this.pos = new Position(x, y);
+		this.width = width;
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public Position getPos() {
+		return pos;
+	}
+
+	/**
+	 * This method will check to see if the new x and y being parsed in is
+	 * within the boundaries of the hitbox.
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public boolean contains(int x, int y) {
+		if 	(pos.getX() <= x && pos.getX() + width >= x &&
+			pos.getY() <= y && pos.getY() + height >= y) {
+			return true;
+		}
+		return false;
+	}
+
+}
