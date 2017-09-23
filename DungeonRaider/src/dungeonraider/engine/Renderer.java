156b0a44460c4f22c8e71b564fab64acf969e380
Jono Yan <jono2496@gmail.com>
2017-09-23 13:31:21 +1200

    something

--- DungeonRaider/src/dungeonraider/engine/Renderer.java
+++ DungeonRaider/src/dungeonraider/engine/Renderer.java
@@ -4,6 +4,8 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 
+import dungeonraider.util.HitBox;
+
 /**
  * Renders view to window
  *
@@ -13,11 +15,12 @@ import java.awt.image.DataBufferInt;
 public class Renderer {
 	private BufferedImage view;
 	private int[] pixels;
-	private boolean black;
+	private HitBox camera;
 
 	public Renderer(int width, int height) {
 		/** Creates view */
 		this.view = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		this.camera = new HitBox(0, 0, view.getWidth(), view.getHeight());
 		/** Creates array for pixels */
 		pixels = ((DataBufferInt) view.getRaster().getDataBuffer()).getData();
 
@@ -27,7 +30,6 @@ public class Renderer {
 				pixels[row * width + col] = color;
 			}
 		}
-		this.black = true;
 
 	}
 
