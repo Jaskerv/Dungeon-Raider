84c11a4028cf96348a9b547e1b707957815449e1
Jono Yan <jono2496@gmail.com>
2017-09-27 23:54:44 +1300

    Added new methods: moveCamera and getCenter to Camera.java

--- DungeonRaider/src/dungeonraider/util/Camera.java
+++ DungeonRaider/src/dungeonraider/util/Camera.java
@@ -5,4 +5,28 @@ public class Camera extends Box {
 		super(x, y, width, height);
 	}
 
+	/**
+	 * This moves the camera relative to its current position. example of 10,0 will
+	 * move the camera 10 px to the right
+	 * 
+	 * @param dx
+	 *            +ve dx will move camera right, -ve dx will move camera left
+	 * @param dy
+	 *            +ve dy will move camera down, -ve dy will move camera up
+	 */
+	public void moveCamera(int dx, int dy) {
+		this.setX(this.x + dx);
+		this.setY(this.y + dy);
+	}
+
+	/**
+	 * Gets center Position of the camera.
+	 * 
+	 * @param width
+	 * @param height
+	 * @return
+	 */
+	public Position getCenter() {
+		return new Position(this.width / 2, this.height / 2);
+	}
 }
