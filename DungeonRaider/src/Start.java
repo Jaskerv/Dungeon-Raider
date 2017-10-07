3ed1813cadda2b6014cb4b4673af8ba8d055cc1d
Harry Nguon <harrynguon@gmail.com>
2017-10-07 15:46:46 +1300

    Createed a main menu mockup. Run Start.java to test it out

---
+++ DungeonRaider/src/Start.java
@@ -0,0 +1,248 @@
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineEvent;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+import dungeonraider.engine.Engine;
+
+/**
+ * Mockup main menu. 
+ * Contains:
+ * 		Firey background image
+ * 		Dungeon Raider logo created on the internet
+ * 		DRM-Free main menu music 
+ * 
+ * @see https://www.escapemotions.com/experiments/flame/ - Background image
+ * @see http://flamingtext.com/ "Blackbird" - Title
+ * @see https://www.reddit.com/r/gamedev/comments/6y699a
+ * 			/i_have_released_my_1363_songs_free_under_creative/ 
+ * 			"rollwithit.mp3" - Music
+ * 
+ * @author harry
+ *
+ */
+public class Start extends JFrame implements KeyListener, MouseListener {
+	
+	private static final int WIDTH = 1280;
+	private static final int HEIGHT = 720;
+	private Image backgroundImage;
+	private Image title;
+	/** Audio clip */
+	private Clip clip;
+	/** This boolean indicates if the user has pressed a button */
+	private boolean active = false;
+	/** First key press */
+	private boolean firstKeyPress = true;
+	/** Selection from 'Play' to 'Quit' */
+	private int[] menuSelection = new int[3];
+	private int index = 0;
+
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Initialises the main menu frame, reads the resources and adds the key
+	 * listener
+	 * @param _title
+	 */
+	public Start(String _title) {
+		super(_title);
+		menuSelection[0] = 200;
+		menuSelection[1] = 350;
+		menuSelection[2] = 500;
+		try {
+			backgroundImage = ImageIO
+					.read(new FileInputStream("resources/images/FlameArtwork.jpg"));
+			title = ImageIO
+					.read(new FileInputStream("resources/images/DungeonRaider.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		addKeyListener(this);
+		setFocusable(true);
+	}
+	
+	/**
+	 * This method plays the main menu music and draws to the JPanel that
+	 * is created within the Frame
+	 */
+	public void execute() {
+		playMusic();
+		JPanel drawingPanel = new JPanel() {
+			private static final long serialVersionUID = 1L;
+			@Override 
+			public void paintComponent(Graphics _g) {
+				super.paintComponent(_g);
+				Graphics2D g = (Graphics2D) _g;
+				//font smoothening
+				RenderingHints rh =
+						new RenderingHints(RenderingHints.KEY_ANTIALIASING, 
+								RenderingHints.VALUE_ANTIALIAS_ON);
+
+				rh.put(RenderingHints.KEY_RENDERING,
+						RenderingHints.VALUE_RENDER_QUALITY);
+				g.setRenderingHints(rh);
+				g.setFont(new Font("Purisa", Font.PLAIN, 35));
+				g.setColor(Color.gray.brighter());
+				g.drawImage(backgroundImage, 0, 0, 1280, 720, null);
+				g.drawImage(title, 300, 30, title.getWidth(null), 
+						title.getHeight(null), null);
+				
+				if (active) {
+					g.drawString("Play", 600, 250);
+					g.drawString("Info", 597, 400);
+					g.drawString("Quit", 600, 550);
+					g.setColor(Color.red.darker());
+					g.setStroke(new BasicStroke(3));
+					g.drawOval(578, menuSelection[index], 120, 70);
+				}
+				else {
+					g.setFont(new Font("Purisa", Font.PLAIN, 20));
+					g.drawString("PRESS ANY KEY TO CONTINUE", 480, 350);
+				}
+			}
+		};
+		this.setContentPane(drawingPanel);
+		this.validate();
+		setSize(WIDTH, HEIGHT);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	/**
+	 * This method plays the music when the user is in the main menu
+	 */
+	public void playMusic() {
+		try {
+		    File file = new File("resources/images/music.wav");
+		    AudioInputStream stream;
+		    AudioFormat format;
+		    DataLine.Info info;
+		    stream = AudioSystem.getAudioInputStream(file);
+		    format = stream.getFormat();
+		    info = new DataLine.Info(Clip.class, format);
+		    clip = (Clip) AudioSystem.getLine(info);
+		    clip.addLineListener(e -> {
+		    	if (e.getType() == LineEvent.Type.STOP) {
+                    clip.close();
+		    	}
+			});
+		    clip.open(stream);
+		    clip.start();
+		}
+		catch (Exception e) {
+			
+		}
+	}
+	
+	/**
+	 * User interaction with the main menu
+	 */
+	@Override
+	public void keyPressed(KeyEvent e) {
+		//Gets rid of the 'press any key to continue' string, and displays
+		//the main menu buttons
+		if (firstKeyPress) {
+			active = true;
+			firstKeyPress = false;
+			this.repaint();
+			return;
+		}
+		active = true;
+		int keyCode = e.getKeyCode();
+		//navigating the main menu
+		if (keyCode == KeyEvent.VK_UP) {
+			if (index > 0) {
+				index--;
+				this.repaint();
+			}
+		} 
+		else if (keyCode == KeyEvent.VK_DOWN) {
+			if (index < 2) {
+				index++;
+				this.repaint();
+			}
+		}
+		else if (keyCode == KeyEvent.VK_ENTER) {
+			//starts the game (new Engine instance)
+			if (index == 0) {
+				//prevents freezing when stopping the music
+				Thread t = new Thread(new Runnable() { 
+				     public void run() {
+				         clip.stop();
+				     }
+				});
+				t.start();
+				this.dispose();
+				//Creates the instance of the game
+				Engine game = new Engine();
+				Thread thread = new Thread(game);
+				thread.start();
+			} 
+			//'Info' button - unimplemented
+			else if (index == 1) {
+				JOptionPane.showMessageDialog(this,
+		                "--Unimplemented--");
+			} 
+			//'Quit' button
+			else if (index == 2) {
+				System.exit(0);
+			}
+		}
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {}
+
+	@Override
+	public void keyReleased(KeyEvent e) {}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {}
+
+	@Override
+	public void mousePressed(MouseEvent e) {}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+
+	@Override
+	public void mouseExited(MouseEvent e) {}
+	
+	/**
+	 * s
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(()
+				-> new Start("Dungeon Raider").execute());
+	}
+
+}
