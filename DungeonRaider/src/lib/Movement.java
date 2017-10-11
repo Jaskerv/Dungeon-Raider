075a45fd42940167694e777e0dc9d8b24f4e03c6
ngcharnon <charnonng1997@gmail.com>
2017-10-12 00:31:59 +1300

    updated the movement and movementests classes in the lib package

--- DungeonRaider/src/lib/Movement.java
+++ DungeonRaider/src/lib/Movement.java
@@ -1,26 +1,33 @@
-
 package lib;
+
 
 public class Movement {
 
 
 	public static int moveLeft(int currentX) {
-		//return currentX - 3;
-		throw new UnsupportedOperationException();
+		if(currentX <= 3) {
+			return 0;
+		}
+		return currentX - 3;
+		
 	}
 
 	public static int moveRight(int currentX) {
-		//return currentX + 3;
-		throw new UnsupportedOperationException();
+		//need to figure out right wall coordinate
+		return currentX + 3;
+		
 	}
 
 	public static int moveUp(int currentY) {
-		//return currentY - 3;
-		throw new UnsupportedOperationException();
+		if(currentY <= 3) {
+			return 0;
+		}
+		return currentY - 3;
+		
 	}
 
 	public static int moveDown(int currentY) {
-		throw new UnsupportedOperationException();
+		return currentY + 3;
 	}
 
 
@@ -28,23 +35,22 @@ public class Movement {
 	//--------------------------------------------------------------------------
 
 
-	public static int moveUpRight(int currentX) {
-		//return currentX - 3;
-		throw new UnsupportedOperationException();
-	}
-
-	public static int moveUpLeft(int currentX) {
-		//return currentX + 3;
-		throw new UnsupportedOperationException();
-	}
-
-	public static int moveDownRight(int currentY) {
-		//return currentY - 3;
-		throw new UnsupportedOperationException();
-	}
-
-	public static int moveDownLeft(int currentY) {
-		throw new UnsupportedOperationException();
-	}
+//	public static int moveUpRight(int currentX) {
+//		
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	public static int moveUpLeft(int currentX) {
+//		
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	public static int moveDownRight(int currentY) {
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	public static int moveDownLeft(int currentY) {
+//		throw new UnsupportedOperationException();
+//	}
 
 }
