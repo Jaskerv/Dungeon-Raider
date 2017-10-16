cfd15806666183e2aa84373e273ef6030a31d488
Jono Yan <jono2496@gmail.com>
2017-10-16 21:40:03 +1300

    Updated player

--- DungeonRaider/src/gameEngine/character/Player.java
+++ DungeonRaider/src/gameEngine/character/Player.java
@@ -1,5 +1,6 @@
 package gameEngine.character;
 
+import java.awt.Color;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -17,11 +18,12 @@ import gameEngine.sprite.AnimatedSprite;
 import gameEngine.sprite.Sprite;
 import gameEngine.util.Box;
 import gameEngine.util.Position;
-
+import gameEngine.util.Rectangle;
 
 /**
- * Need to implement equiping of weapon then test if attacking works because right now null pointer exeception
- * when trying to find weapon range
+ * Need to implement equiping of weapon then test if attacking works because
+ * right now null pointer exeception when trying to find weapon range
+ * 
  * @author Gabriel Tennent
  *
  */
@@ -41,19 +43,18 @@ public class Player implements Character, GameObject {
 	private Weapon primaryWeapon;
 	private Inventory inventory;
 	private Sprite spriteImage;
-	private Box playerBoundBox;
+	private Rectangle playerBoundBox;
 	private Box pickUpRadius;
 
 	public static final int SPEED = 2;
 	public static final int SPRINT = 7;
 	private int zoom;
 	private Queue<Integer> damageQueue;
-	
+
 	private Sprite sprite;
 	private AnimatedSprite animatedSprite = null;
-	//0 == right, 1 == left, 2 == up, 3 == down. This is based off the Player.png
+	// 0 == right, 1 == left, 2 == up, 3 == down. This is based off the Player.png
 	private int direction = 0;
-
 
 	public Player(Position center, int stamina, Sprite playerSprite, int zoom, int hp, int hpMax) {
 		this.damageQueue = new PriorityQueue<>();
@@ -62,28 +63,34 @@ public class Player implements Character, GameObject {
 		this.zoom = 3;
 		this.x = center.getX() - (playerSprite.getWidth() / 2 * zoom);
 		this.y = center.getY() - (playerSprite.getHeight() / 2 * zoom);
+		System.out.println(x + " " + y);
 		this.hp = hp;
 		this.hpMax = hpMax;
-		this.playerBoundBox = new Box(x, y, playerSprite.getWidth()*zoom, playerSprite.getHeight()*zoom);
-		//Pick up radius of the player starts half of the players width to the left of the player and
-		//extends to twice the players width meaning the pick up radius is twice the size of the player
-		this.pickUpRadius	= new Box(x-((playerSprite.getWidth()*zoom)), y-((playerSprite.getHeight()*zoom)),
-				(playerSprite.getWidth()*zoom)*4, (playerSprite.getHeight()*zoom)*4);
+
+		// Pick up radius of the player starts half of the players width to the left of
+		// the player and
+		// extends to twice the players width meaning the pick up radius is twice the
+		// size of the player
+		this.pickUpRadius = new Box(x - ((playerSprite.getWidth() * zoom)), y - ((playerSprite.getHeight() * zoom)),
+				(playerSprite.getWidth() * zoom) * 4, (playerSprite.getHeight() * zoom) * 4);
 		this.inventory = new Inventory(20);
 		this.primaryEquipped = true;
 		this.primaryWeapon = new Weapon("Start", 0, 0, 10, 300, 10, playerSprite);
 		this.sprite = playerSprite;
-		if(sprite != null && sprite instanceof AnimatedSprite) {
+		if (sprite != null && sprite instanceof AnimatedSprite) {
 			this.animatedSprite = (AnimatedSprite) playerSprite;
 		}
+		this.playerBoundBox = new Rectangle(x+10, y+63, animatedSprite.getWidth(), (int) (animatedSprite.getHeight() * 0.4));
+		this.playerBoundBox.generateGraphics(Color.blue.getRGB());
 		updateDirection();
 	}
 
 	private void updateDirection() {
-		if(animatedSprite != null) {
-			animatedSprite.setAnimationRange( direction*8, direction * 8 + 7 );
-		} 
+		if (animatedSprite != null) {
+			animatedSprite.setAnimationRange(direction * 8, direction * 8 + 7);
+		}
 	}
+
 	@Override
 	public int heavyAttack() {
 		return primaryWeapon.getDamage();
@@ -92,45 +99,45 @@ public class Player implements Character, GameObject {
 	@Override
 	public void walkLeft() {
 		this.x -= SPEED;
-		this.playerBoundBox.setX(this.x-=SPEED);
+		this.playerBoundBox.setX(this.playerBoundBox.getX() - SPEED);
 	}
 
 	@Override
 	public void walkRight() {
 		this.x += SPEED;
-		this.playerBoundBox.setX(this.x+=SPEED);
+		this.playerBoundBox.setX(this.playerBoundBox.getX() + SPEED);
 	}
 
 	@Override
 	public void walkUp() {
 		this.y -= SPEED;
-		this.playerBoundBox.setY(this.y-=SPEED);
+		this.playerBoundBox.setY(this.playerBoundBox.getY() - SPEED);
 	}
 
 	@Override
 	public void walkDown() {
 		this.y += SPEED;
-		this.playerBoundBox.setY(this.y+=SPEED);
+		this.playerBoundBox.setY(this.playerBoundBox.getY() + SPEED);
 	}
 
 	public void runLeft() {
 		this.x -= SPRINT;
-		this.playerBoundBox.setX(this.x-=SPRINT);
+		this.playerBoundBox.setX(this.playerBoundBox.getX() - SPRINT);
 	}
 
 	public void runRight() {
 		this.x += SPRINT;
-		this.playerBoundBox.setX(this.x += SPRINT);
+		this.playerBoundBox.setX(this.playerBoundBox.getX() + SPRINT);
 	}
 
 	public void runUp() {
 		this.y -= SPRINT;
-		this.playerBoundBox.setY(this.y -= SPRINT);
+		this.playerBoundBox.setY(this.playerBoundBox.getY() - SPRINT);
 	}
 
 	public void runDown() {
 		this.y += SPRINT;
-		this.playerBoundBox.setY(this.y += SPRINT);
+		this.playerBoundBox.setY(this.playerBoundBox.getY() + SPRINT);
 	}
 
 	public void interact() {
@@ -142,20 +149,24 @@ public class Player implements Character, GameObject {
 	 */
 	@Override
 	public void render(Renderer renderer, int xZoom, int yZoom) {
-		//renderer.renderArray(spriteImage.getPixels(), spriteImage.getWidth(), spriteImage.getWidth(), x, y, zoom, zoom);
-		
-		//introducing the animated sprite here. initially rendering a static sprite.
-		if(animatedSprite != null)
-			renderer.renderSprite(animatedSprite, playerBoundBox.getX(), playerBoundBox.getY(), xZoom, yZoom);
-		else if(sprite != null)
-			renderer.renderSprite(sprite, playerBoundBox.getX(), playerBoundBox.getY(), xZoom, yZoom);
+		// renderer.renderArray(spriteImage.getPixels(), spriteImage.getWidth(),
+		// spriteImage.getWidth(), x, y, zoom, zoom);
+
+		// introducing the animated sprite here. initially rendering a static sprite.
+		renderer.renderRectangle(playerBoundBox, xZoom, yZoom);
+		if (animatedSprite != null)
+			renderer.renderSprite(animatedSprite, x + animatedSprite.getWidth() / 2, y + animatedSprite.getHeight()/2, xZoom, yZoom);
+		else if (sprite != null)
+			renderer.renderSprite(sprite, x + animatedSprite.getWidth() / 2, y + animatedSprite.getHeight() / 2, xZoom,
+					yZoom);
 		else
-			renderer.renderArray(spriteImage.getPixels(), spriteImage.getWidth(), spriteImage.getWidth(), x, y, zoom, zoom);
+			renderer.renderArray(spriteImage.getPixels(), spriteImage.getWidth(), spriteImage.getWidth(), x, y, zoom,
+					zoom);
 	}
 
 	/**
-	 * Updates the player dependant on the key and mouse controllers in the engine and what booleans have been activated
-	 * via the user pushing keys / the mouse.
+	 * Updates the player dependant on the key and mouse controllers in the engine
+	 * and what booleans have been activated via the user pushing keys / the mouse.
 	 */
 	@Override
 	public void update(Engine engine) {
@@ -163,45 +174,45 @@ public class Player implements Character, GameObject {
 		Map currentMap = engine.getCurrentMap();
 		MouseController mouseActions = engine.getMouseListener();
 
-		int width = spriteImage.getWidth()*zoom;
-		int height = spriteImage.getHeight()*zoom;
+		int width = spriteImage.getWidth() * zoom;
+		int height = spriteImage.getHeight() * zoom;
 
-		//for the player animated sprites
+		// for the player animated sprites
 		boolean didMove = false;
 		int newDirection = direction;
-		
+
 		/**
-		 * Player walking connection with key controller:
-		 * Also checks for player connection with walls
+		 * Player walking connection with key controller: Also checks for player
+		 * connection with walls
 		 */
-		if(!keyBinds.isRun()) {
+		if (!keyBinds.isRun()) {
 			if (keyBinds.isUp()) {
-				if(checkBoundry(currentMap, x + width, y - SPEED + height/2))
-					if(checkBoundry(currentMap, x, y - SPEED + height/2)) {
+				if (checkBoundry(currentMap, x + width, y - SPEED + height / 2))
+					if (checkBoundry(currentMap, x, y - SPEED + height / 2)) {
 						newDirection = 2;
 						didMove = true;
 						walkUp();
 					}
 			}
 			if (keyBinds.isDown()) {
-				if(checkBoundry(currentMap, x + width, y + height + SPEED))
-					if(checkBoundry(currentMap, x, y + height + SPEED)) {
+				if (checkBoundry(currentMap, x + width, y + height + SPEED))
+					if (checkBoundry(currentMap, x, y + height + SPEED)) {
 						newDirection = 3;
 						didMove = true;
 						walkDown();
 					}
 			}
 			if (keyBinds.isLeft()) {
-				if(checkBoundry(currentMap, x - SPEED, y + height/2))
-					if(checkBoundry(currentMap, x - SPEED, y + height)) {
+				if (checkBoundry(currentMap, x - SPEED, y + height / 2))
+					if (checkBoundry(currentMap, x - SPEED, y + height)) {
 						newDirection = 1;
 						didMove = true;
 						walkLeft();
 					}
 			}
 			if (keyBinds.isRight()) {
-				if(checkBoundry(currentMap, x + width + SPEED, y + height/2))
-					if(checkBoundry(currentMap, x + width + SPEED, y + height)) {
+				if (checkBoundry(currentMap, x + width + SPEED, y + height / 2))
+					if (checkBoundry(currentMap, x + width + SPEED, y + height)) {
 						newDirection = 0;
 						didMove = true;
 						walkRight();
@@ -210,37 +221,37 @@ public class Player implements Character, GameObject {
 		}
 
 		/**
-		 * Player running connection with key controller:
-		 * Also checks for player connection with walls
+		 * Player running connection with key controller: Also checks for player
+		 * connection with walls
 		 */
-		if(keyBinds.isRun()) {
+		if (keyBinds.isRun()) {
 			if (keyBinds.isUp()) {
-				if(checkBoundry(currentMap, x + width, y - SPRINT + height/2))
-					if(checkBoundry(currentMap, x, y - SPRINT + height/2)) {
+				if (checkBoundry(currentMap, x + width, y - SPRINT + height / 2))
+					if (checkBoundry(currentMap, x, y - SPRINT + height / 2)) {
 						newDirection = 2;
 						didMove = true;
 						runUp();
 					}
 			}
 			if (keyBinds.isDown()) {
-				if(checkBoundry(currentMap, x + width, y + height + SPRINT))
-					if(checkBoundry(currentMap, x, y + height + SPRINT)) {
+				if (checkBoundry(currentMap, x + width, y + height + SPRINT))
+					if (checkBoundry(currentMap, x, y + height + SPRINT)) {
 						newDirection = 3;
 						didMove = true;
 						runDown();
 					}
 			}
 			if (keyBinds.isLeft()) {
-				if(checkBoundry(currentMap, x - SPRINT, y + height/2))
-					if(checkBoundry(currentMap, x - SPRINT, y + height)) {
+				if (checkBoundry(currentMap, x - SPRINT, y + height / 2))
+					if (checkBoundry(currentMap, x - SPRINT, y + height)) {
 						newDirection = 1;
 						didMove = true;
 						runLeft();
 					}
 			}
 			if (keyBinds.isRight()) {
-				if(checkBoundry(currentMap, x + width + SPRINT, y + height/2))
-					if(checkBoundry(currentMap, x + width + SPRINT, y + height)) {
+				if (checkBoundry(currentMap, x + width + SPRINT, y + height / 2))
+					if (checkBoundry(currentMap, x + width + SPRINT, y + height)) {
 						newDirection = 0;
 						didMove = true;
 						runRight();
@@ -248,15 +259,15 @@ public class Player implements Character, GameObject {
 			}
 		}
 
-		//Checking if player is attempting to pick up and whether there is anything to pick up
-		if(keyBinds.isPickUp()) {
-			this.pickUpRadius	= new Box(x, y,
-					(spriteImage.getWidth()*zoom), (spriteImage.getHeight()*zoom));
-			//need to check if any of the items locations
+		// Checking if player is attempting to pick up and whether there is anything to
+		// pick up
+		if (keyBinds.isPickUp()) {
+			this.pickUpRadius = new Box(x, y, (spriteImage.getWidth() * zoom), (spriteImage.getHeight() * zoom));
+			// need to check if any of the items locations
 			List<Item> itemsOnMap = engine.getCurrentMap().getItems();
-			for(Item item : itemsOnMap) {
+			for (Item item : itemsOnMap) {
 				Box itemPos = item.getBoundingBox();
-				if(pickUpRadius.contains(itemPos)){
+				if (pickUpRadius.contains(itemPos)) {
 					this.inventory.add(item);
 					item.setPickedUp(true);
 					itemsOnMap.remove(item);
@@ -266,33 +277,35 @@ public class Player implements Character, GameObject {
 		}
 
 		/**
-		 * Returns left clicks and their co-ordinates to tell where for the character to attack
+		 * Returns left clicks and their co-ordinates to tell where for the character to
+		 * attack
 		 */
-		while(!mouseActions.getAttackPositions().isEmpty()) {
-			//Gets the attack position from the mouse controller
+		while (!mouseActions.getAttackPositions().isEmpty()) {
+			// Gets the attack position from the mouse controller
 			Position attackPos = mouseActions.getAttackPos();
-			//Prevents player from being able to run and attack at the same time
+			// Prevents player from being able to run and attack at the same time
 			keyBinds.setRun(false);
 			int mx = attackPos.getX();
 			int my = attackPos.getY();
-			if(primaryEquipped)	attack(mx, my, engine);
+			if (primaryEquipped)
+				attack(mx, my, engine);
 		}
 
 		/**
 		 * Updates the players hpbar if they have taken damage
 		 */
-		if(!damageQueue.isEmpty()) {
+		if (!damageQueue.isEmpty()) {
 			int damage = damageQueue.poll();
 			this.hp += damage;
 		}
-		
+
 		/**
 		 * Attempts to use a players item to heal the player
 		 */
-		if(keyBinds.isUseItem()) {
-			if(!this.inventory.getInventory().isEmpty()) {
-			Consumable healthPot = (Consumable) this.inventory.returnFirstItem();
-			this.heal(healthPot.getHealingStrength());
+		if (keyBinds.isUseItem()) {
+			if (!this.inventory.getInventory().isEmpty()) {
+				Consumable healthPot = (Consumable) this.inventory.returnFirstItem();
+				this.heal(healthPot.getHealingStrength());
 			}
 			keyBinds.setUseItem(false);
 		}
@@ -300,24 +313,24 @@ public class Player implements Character, GameObject {
 		/**
 		 * Updates camera
 		 */
-		//only update the direction if the player moves in a different direction
-		if(newDirection != direction) {
+		// only update the direction if the player moves in a different direction
+		if (newDirection != direction) {
 			direction = newDirection;
 			updateDirection();
 		}
-		
-		if(!didMove) {
-			//makes sure that the sprite doesnt stop mid movement
+
+		if (!didMove) {
+			// makes sure that the sprite doesnt stop mid movement
 			animatedSprite.reset();
 		}
-		
+
 		this.updateCamera(engine.getRenderer().getCamera());
-		
-		//update the counter 
-		if(didMove) {
+
+		// update the counter
+		if (didMove) {
 			animatedSprite.update(engine);
 		}
-		
+
 		if (this.x >= 1940 && this.y <= 100) {
 			engine.setCurrentMap(engine.getMapList().get(engine.getCurrentMapNumber()));
 			this.x = 200;
@@ -337,44 +350,45 @@ public class Player implements Character, GameObject {
 		int height = spriteImage.getHeight() * zoom;
 		Position center = engine.getRenderer().getCamera().getCenter();
 		List<GameObject> monsters = engine.getCurrentMap().getMonsters();
-		//right attack calculated from the position of the camera
-		if(mx > center.getX()) {
-			//right side bounding box = to range of weapon and half player height
+		// right attack calculated from the position of the camera
+		if (mx > center.getX()) {
+			// right side bounding box = to range of weapon and half player height
 			Box rightPrimaryAttackRad = new Box(x, y, primaryWeapon.getRange(), height);
-			//attackRight();
-			for(GameObject _monster : monsters) {
+			// attackRight();
+			for (GameObject _monster : monsters) {
 				Monster monster = (Monster) _monster;
-				if(rightPrimaryAttackRad.contains(monster.getBoundingBox())) {
+				if (rightPrimaryAttackRad.contains(monster.getBoundingBox())) {
 					monster.damage(heavyAttack());
 					System.out.println(monster.getHealth());
-					checkForMonsterDeath(monster,monsters);
+					checkForMonsterDeath(monster, monsters);
 				}
 			}
 		}
-		//left attack calculated from the position of the camera
-		if(mx < center.getX()) {
-			//left side bounding box = to range of weapon and half player height
-			Box leftPrimaryAttackRad = new Box(x-primaryWeapon.getRange(), y, primaryWeapon.getRange()+width, height);
-			//attackLeft();
-			for(GameObject _monster : monsters) {
+		// left attack calculated from the position of the camera
+		if (mx < center.getX()) {
+			// left side bounding box = to range of weapon and half player height
+			Box leftPrimaryAttackRad = new Box(x - primaryWeapon.getRange(), y, primaryWeapon.getRange() + width,
+					height);
+			// attackLeft();
+			for (GameObject _monster : monsters) {
 				Monster monster = (Monster) _monster;
-				if(leftPrimaryAttackRad.contains(monster.getBoundingBox())) {
+				if (leftPrimaryAttackRad.contains(monster.getBoundingBox())) {
 					monster.damage(heavyAttack());
 					System.out.println(monster.getHealth());
-					checkForMonsterDeath(monster,monsters);
+					checkForMonsterDeath(monster, monsters);
 				}
 			}
 		}
 	}
-	
+
 	public void checkForMonsterDeath(Monster monster, List<GameObject> monsters) {
-		if(monster.getHealth() <= 0 ) {
+		if (monster.getHealth() <= 0) {
 			this.gold = this.gold + 1;
-			removeMonsterFromMap(monster,monsters);
+			removeMonsterFromMap(monster, monsters);
 		}
-		
+
 	}
-	
+
 	public void removeMonsterFromMap(Monster monster, List<GameObject> monsters) {
 		monsters.remove(monster);
 	}
@@ -388,7 +402,6 @@ public class Player implements Character, GameObject {
 		camera.setX(x - (camera.getWidth() / 2) + (this.spriteImage.getWidth() * zoom / 2));
 		camera.setY(y - (camera.getHeight() / 2) + (this.spriteImage.getHeight() * zoom / 2));
 	}
-
 
 	/**
 	 * Getters and setters for the player class
@@ -407,10 +420,12 @@ public class Player implements Character, GameObject {
 	public int getHpMax() {
 		return hpMax;
 	}
-	
+
 	public boolean isDead() {
-		if(hp <= 0) return true;
-		else return false;
+		if (hp <= 0)
+			return true;
+		else
+			return false;
 	}
 
 	/**
@@ -421,13 +436,13 @@ public class Player implements Character, GameObject {
 	public void damage(int i) {
 		this.damageQueue.offer(-i);
 	}
-	
+
 	/**
 	 * Heals player with negative numbers
 	 *
 	 * @param i
 	 */
-	public void heal(int i ) {
+	public void heal(int i) {
 		this.damageQueue.offer(i);
 	}
 
@@ -444,7 +459,8 @@ public class Player implements Character, GameObject {
 	}
 
 	public void collectgold(int amountRecieved) {
-		gold = gold + amountRecieved;;
+		gold = gold + amountRecieved;
+		;
 	}
 
 	public int getX() {
@@ -480,7 +496,7 @@ public class Player implements Character, GameObject {
 	}
 
 	public void setPlayerBoundBox(Box playerBoundBox) {
-		this.playerBoundBox = playerBoundBox;
+		this.playerBoundBox = (Rectangle) playerBoundBox;
 	}
 
 	public Inventory getInventory() {
@@ -490,6 +506,5 @@ public class Player implements Character, GameObject {
 	public void setInventory(Inventory inventory) {
 		this.inventory = inventory;
 	}
-
 
 }
