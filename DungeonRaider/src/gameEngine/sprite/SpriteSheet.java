bff46847443a4c2b79adfd337539e6b50ba477cf
Charnon Ng <charnonng1997@gmail.com>
2017-10-19 13:57:56 +1300

    cleaned up the SpriteSheet class

--- DungeonRaider/src/gameEngine/sprite/SpriteSheet.java
+++ DungeonRaider/src/gameEngine/sprite/SpriteSheet.java
@@ -29,14 +29,22 @@ public class SpriteSheet {
 		this.pixels = sheetImage.getRGB(0, 0, SIZEX, SIZEY, pixels, 0, SIZEX);
 	}
 
+	/**
+	 * Separates and loads sprites into the loadedSprites field
+	 *
+	 * @param spriteSizeX The width of the sprite sheet
+	 * @param spriteSizeY The height of the sprite sheet
+	 */
 	public void loadSprites(int spriteSizeX, int spriteSizeY) {
 		this.spriteSizeX = spriteSizeX;
-		loadedSprites = new Sprite[(SIZEX / spriteSizeX) * (SIZEY / spriteSizeY)];
+		loadedSprites = new Sprite[(SIZEX / spriteSizeX)
+				* (SIZEY / spriteSizeY)];
 
 		int spriteID = 0;
 		for (int y = 0; y < SIZEY; y += spriteSizeY) {
 			for (int x = 0; x < SIZEX; x += spriteSizeX) {
-				loadedSprites[spriteID] = new Sprite(this, x, y, spriteSizeX, spriteSizeY);
+				loadedSprites[spriteID] = new Sprite(this, x, y, spriteSizeX,
+						spriteSizeY);
 				spriteID++;
 			}
 		}
@@ -44,34 +52,52 @@ public class SpriteSheet {
 		spritesLoaded = true;
 	}
 
+	/**
+	 * returns a sprite gathered from the loadedSprite field from the given x
+	 * and y coordinates
+	 *
+	 * @param x
+	 *            The column of the sprite that is returned
+	 * @param y
+	 *            The row of the sprite that is returned
+	 * @return The sprite from the given x and y coordinates
+	 */
 	public Sprite getSprite(int x, int y) {
 		if (spritesLoaded) {
 			int spriteID = x + y * (SIZEX / spriteSizeX);
 
-			if (spriteID < loadedSprites.length)
+			if (spriteID < loadedSprites.length) {
 				return loadedSprites[spriteID];
-			else
-				System.out.println(
-						"SpriteID of " + spriteID + " is out of bounds with a length of " + loadedSprites.length + ".");
-		} else
-			System.out.println("SpriteSheet could not get a sprite with no loaded sprites.");
+			} else {
+				System.out.println("SpriteID of " + spriteID
+						+ " is out of bounds with a length of "
+						+ loadedSprites.length + ".");
+			}
+
+		} else {
+			System.out.println(
+					"SpriteSheet could not get a sprite with no loaded sprites.");
+		}
 
 		return null;
 	}
 
+	/**
+	 * @return The array of sprites gathered from the sprite sheet.
+	 */
 	public Sprite[] getLoadedSprites() {
 		return loadedSprites;
 	}
 
 	/**
-	 * @return the pixels
+	 * @return the array of int representing the pixels
 	 */
 	public int[] getPixels() {
 		return pixels;
 	}
 
 	/**
-	 * @return the sheet
+	 * @return the bufferedImage of sprites
 	 */
 	public BufferedImage getSheet() {
 		return sheet;
